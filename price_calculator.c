c387547fd38a1062c9ea82c6bf6bfe6aa870bb49
Ahmed Melli <114910552+MrMelli@users.noreply.github.com>
2024-01-05 11:17:21 +0100

    Update price_calculator.c

--- price_calculator.c
+++ price_calculator.c
@@ -1,5 +1,6 @@
 #include "price_calculator.h"
 
-float calculatePrice(char product_id[11], float base_price, char person_id[11], int day) {
-    // Implementera calculatePrice som tidigare nämnts
+float calculatePrice(char product_id[11], float base_price, char person_id[11], int day)
+{
+    
 }
